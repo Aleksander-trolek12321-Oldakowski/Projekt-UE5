e56f8ff5150df79d6c7f6233877b042d1cab916b
Aleksander Ołdakowski <aleks.oldakowski@wp.pl>
2025-11-13 17:59:09 +0100

    BaseClasses

--- Source/Projekt_UE5/ABasePlayerCharacter.cpp
+++ Source/Projekt_UE5/ABasePlayerCharacter.cpp
@@ -2,4 +2,64 @@
 
 
 #include "ABasePlayerCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
+#include "GameFramework/PlayerController.h"
 
+AABasePlayerCharacter::AABasePlayerCharacter()
+{
+    PrimaryActorTick.bCanEverTick = true;
+}
+
+
+
+void AABasePlayerCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (APlayerController* PC = Cast<APlayerController>(GetController()))
+    {
+        if (ULocalPlayer* LP = PC->GetLocalPlayer())
+        {
+            if (UEnhancedInputLocalPlayerSubsystem* Subsystem = LP->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>())
+            {
+                if (DefaultMappingContext)
+                {
+                    // priorytet 0 (możesz zmienić)
+                    Subsystem->AddMappingContext(DefaultMappingContext, 0);
+                }
+            }
+        }
+    }
+}
+
+void AABasePlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    if (UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+    {
+        if (IA_Move)     EIC->BindAction(IA_Move, ETriggerEvent::Triggered, this, &AABasePlayerCharacter::Move);
+        if (IA_Interact) EIC->BindAction(IA_Interact, ETriggerEvent::Started,   this, &AABasePlayerCharacter::OnInteract);
+        if (IA_Attack)   EIC->BindAction(IA_Attack, ETriggerEvent::Started,     this, &AABasePlayerCharacter::OnAttack);
+    }
+}
+
+void AABasePlayerCharacter::Move(const FInputActionValue& Value)
+{
+    FVector2D MovementVector = Value.Get<FVector2D>();
+    AddMovementInput(GetActorForwardVector(), MovementVector.Y);
+    AddMovementInput(GetActorRightVector(),   MovementVector.X);
+}
+
+void AABasePlayerCharacter::OnInteract(const FInputActionValue& Value)
+{
+    UE_LOG(LogTemp, Log, TEXT("Interact pressed"));
+}
+
+void AABasePlayerCharacter::OnAttack(const FInputActionValue& Value)
+{
+    UE_LOG(LogTemp, Log, TEXT("Attack pressed"));
+}
--- Source/Projekt_UE5/ABasePlayerCharacter.h
+++ Source/Projekt_UE5/ABasePlayerCharacter.h
@@ -4,14 +4,38 @@
 
 #include "CoreMinimal.h"
 #include "ABaseCharacter.h"
+#include "InputActionValue.h"
 #include "ABasePlayerCharacter.generated.h"
 
-/**
- * 
- */
+class UInputAction;
+class UInputMappingContext;
 UCLASS()
 class PROJEKT_UE5_API AABasePlayerCharacter : public AABaseCharacter
 {
 	GENERATED_BODY()
-	
+
+	public:
+	AABasePlayerCharacter();
+
+	protected:
+		virtual void BeginPlay() override;
+		virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+		// handlers
+		void Move(const FInputActionValue& Value);
+		void OnInteract(const FInputActionValue& Value);
+		void OnAttack(const FInputActionValue& Value);
+
+	public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+    UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+    UInputAction* IA_Move;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+    UInputAction* IA_Interact;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Input")
+    UInputAction* IA_Attack;
 };
